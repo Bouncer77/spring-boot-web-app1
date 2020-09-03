3756c996a9e869baf5d1da7c5e0ffc46e94aa7cf
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-09-03 20:34:17 +0300

    Добавлена авторизация пользователя

---
+++ src/main/java/com/bouncer77/springbootapp1/config/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.bouncer77.springbootapp1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author Kosenkov Ivan
+ * Created by Kosenkov Ivan on 03.09.2020
+ */
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
