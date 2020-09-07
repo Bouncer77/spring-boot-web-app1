9b5a5137b798de519009a103de35bee7c742362a
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-09-07 12:48:22 +0300

    Запись через лямбду SpringBootApp1Application

--- src/main/java/com/bouncer77/springbootapp1/SpringBootApp1Application.java
+++ src/main/java/com/bouncer77/springbootapp1/SpringBootApp1Application.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,57 +19,63 @@ import java.util.Collections;
 import java.util.List;
 
 @SpringBootApplication
-public class SpringBootApp1Application implements CommandLineRunner {
+public class SpringBootApp1Application {
 
-    //private static final Logger log = LoggerFactory.getLogger(SpringBootApp1Application.class);
+    private static final Logger log = LoggerFactory.getLogger(SpringBootApp1Application.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp1Application.class, args);
     }
 
-    @Autowired
-    PersonRepository personRepository;
+    /*@Autowired
+    PersonRepository personRepository;*/
 
-    @Override
-    public void run(String... args) {
-
-        // save a few persons
+    /**
+    * Метод возвращает CommandLineRunnerкомпонент , который автоматически запускает код при запуске приложения.
+    * */
+    @Bean
+    public CommandLineRunner demo(PersonRepository personRepository) {
+        return (args) -> {
+            // save a few persons
             // anna = person 1
-        Person anna = new Person("AnnaLogin", "annaEmail@google.com", "123",
-                "Anna", "Goncharova" );
-        anna.setActive(true);
-        anna.setRoles(Collections.singleton(Role.STUDENT));
-        Passport passportAnna = new Passport("111", "456789");
-        anna.setPassport(passportAnna);
-        Phone phoneAnna1 = new Phone("89681110101");
-        Phone phoneAnna2 = new Phone("89681110102");
-        anna.setPhones(Arrays.asList(phoneAnna1, phoneAnna2));
+            Person anna = new Person("AnnaLogin", "annaEmail@google.com", "123",
+                    "Anna", "Goncharova" );
+            anna.setActive(true);
+            anna.setRoles(Collections.singleton(Role.STUDENT));
+            Passport passportAnna = new Passport("111", "456789");
+            anna.setPassport(passportAnna);
+            Phone phoneAnna1 = new Phone("89681110101");
+            Phone phoneAnna2 = new Phone("89681110102");
+            anna.setPhones(Arrays.asList(phoneAnna1, phoneAnna2));
             // dima = person 2
-        Person dima = new Person("DimaLogin", "dimaEmail@google.com", "654321",
-                "Dima", "Ivanov");
-        dima.setActive(true);
-        dima.setRoles(Collections.singleton(Role.STUDENT));
-        Passport passportDima = new Passport("222", "456555");
-        dima.setPassport(passportDima);
-        Phone phoneDima1 = new Phone("89681110201");
-        dima.setPhones(Collections.singletonList(phoneDima1));
+            Person dima = new Person("DimaLogin", "dimaEmail@google.com", "654321",
+                    "Dima", "Ivanov");
+            dima.setActive(true);
+            dima.setRoles(Collections.singleton(Role.STUDENT));
+            Passport passportDima = new Passport("222", "456555");
+            dima.setPassport(passportDima);
+            Phone phoneDima1 = new Phone("89681110201");
+            dima.setPhones(Collections.singletonList(phoneDima1));
 
             // alex = person 3
-        Person alex = new Person("AlexLogin", "alexEmail@gmail.com", "321", "Alex", "Andreev");
-        alex.setActive(true);
-        alex.setRoles(Collections.singleton(Role.STUDENT));
-        Passport passportAlex = new Passport("111", "456000");
-        alex.setPassport(passportAlex);
-        Phone phoneAlex1 = new Phone("89681110301");
-        alex.setPhones(Collections.singletonList(phoneAlex1));
+            Person alex = new Person("AlexLogin", "alexEmail@gmail.com", "321", "Alex", "Andreev");
+            alex.setActive(true);
+            alex.setRoles(Collections.singleton(Role.STUDENT));
+            Passport passportAlex = new Passport("111", "456000");
+            alex.setPassport(passportAlex);
+            Phone phoneAlex1 = new Phone("89681110301");
+            alex.setPhones(Collections.singletonList(phoneAlex1));
 
-        List<Person> people = Arrays.asList(anna, dima, alex);
-        personRepository.saveAll(people);
-        System.out.println("<<<<<<<<<" + personRepository.findAll());
+            List<Person> people = Arrays.asList(anna, dima, alex);
+            personRepository.saveAll(people);
 
-        System.out.println("\n\n");
-        Passport passport = anna.getPassport();
-        System.out.println(passport);
+            // fetch all persons
+            log.info("Persons found with findAll():");
+            log.info("-------------------------------");
+            for (Person person : personRepository.findAll()) {
+                log.info(person.toString());
+            }
+            log.info("");
+        };
     }
-
 }
