642173d753d2942d8147f1ca2f63ef858ca6f1b8
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-09-08 18:36:58 +0300

    Добавлена авторизация пользователя

--- src/main/java/com/bouncer77/springbootapp1/config/WebSecurityConfig.java
+++ src/main/java/com/bouncer77/springbootapp1/config/WebSecurityConfig.java
@@ -1,19 +1,27 @@
 package com.bouncer77.springbootapp1.config;
 
-/*import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import javax.sql.DataSource;
 
-*//**
+/**
  * @author Kosenkov Ivan
  * Created by Kosenkov Ivan on 03.09.2020
- *//*
+ * */
+
+
 
 @Configuration
 @EnableWebSecurity
@@ -22,11 +30,21 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private DataSource dataSource;
 
+    /*@Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/resources/static/**")
+                .anyRequest();
+    }*/
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                    .antMatchers("/", "/addPerson").permitAll()
+                    .antMatchers("/", "/login", "/addPerson").permitAll()
+                    .antMatchers("/*.css", "/images/bouncer77.png").permitAll()
+                    //.antMatchers("person/addPerson").anonymous()
                     .anyRequest().authenticated()
                 .and()
                     .formLogin()
@@ -37,13 +55,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .permitAll();
     }
 
+    @Bean
     @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("u")
+                        .password("p")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    /*@Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
                 .passwordEncoder(NoOpPasswordEncoder.getInstance())
                 .usersByUsernameQuery("select login, password, active from jpa.person where login=?")
                 .authoritiesByUsernameQuery("select p.login, pr.roles from jpa.person p inner join jpa.person_roles pr on p.id = pr.person_id where p.login=?");
-    }
-
-}*/
+    }*/
+}
