302da09637d01177f6947e43f7095f0659700f5e
Ivan Kosenkov <bouncer77rus@gmail.com>
2020-09-11 15:18:28 +0300

    Добавлен вывод сущности book

--- src/main/java/com/bouncer77/springbootapp1/controller/WebsiteBookController.java
+++ src/main/java/com/bouncer77/springbootapp1/controller/WebsiteBookController.java
@@ -3,6 +3,7 @@ package com.bouncer77.springbootapp1.controller;
 import com.bouncer77.springbootapp1.entity.Book;
 import com.bouncer77.springbootapp1.form.ListBookForm;
 import com.bouncer77.springbootapp1.repository.BookRepository;
+import com.bouncer77.springbootapp1.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Kosenkov Ivan
@@ -21,15 +25,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WebsiteBookController {
 
     @Autowired
-    private BookController bookController;
+    private BookService bookService;
 
-    @Autowired
-    private BookRepository bookRepository;
-
-    @GetMapping
+    @GetMapping //(value = "/all")
     public String showBooks(Model model) {
-        model.addAttribute("books", bookRepository.findAll());
-        return "book/books";
+        model.addAttribute("books", bookService.readAll());
+        return "book/allBooks";
     }
 
     @GetMapping("/add")
@@ -41,19 +42,27 @@ public class WebsiteBookController {
         listBookForm.addBook(new Book("Книга 2", "Описание 2", 200));
         listBookForm.addBook(new Book("Книга 3", "Описание 3", 300));
 
-        /*for (int i = 0; i < 3; i++) {
-            listBookForm.addBook(new Book());
-        }*/
-
         model.addAttribute("books", listBookForm.getBooks());
         return "book/addBook";
     }
 
-    @PostMapping("/save")
+    /*@PostMapping("/save")
     public String saveBooks(@ModelAttribute ListBookForm ListBookForm, Model model) {
 
         bookRepository.saveAll(ListBookForm.getBooks());
         model.addAttribute("books", bookRepository.findAll());
         return "redirect:web/books";
     }
+
+    @GetMapping(value = "/edit")
+    public String showEditForm(Model model) {
+        List<Book> books = new ArrayList<>();
+        bookService.findAll()
+                .iterator()
+                .forEachRemaining(books::add);
+
+        model.addAttribute("form", new BooksCreationDto(books));
+
+        return "editBooksForm";
+    }*/
 }
